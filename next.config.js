69cd160b2dc27575a608d70b4ee0f3a180480354
Steven Rueda <ruedabus1@yahoo.com>
2026-04-10 12:27:50 -0400

    push to vercel completed strip integration2

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false,
-  },
   webpack(config) {
     config.resolve.modules = [
       path.resolve(__dirname, "node_modules"),
